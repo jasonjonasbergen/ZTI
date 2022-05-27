3fbd74005eba2a519050e8160874cb49f3b7825c
jasonjonasbergen <93658825+jasonjonasbergen@users.noreply.github.com>
2022-05-27 09:41:24 -0500

    Update Win10-Enterprise-21H2.ps1

--- Win10-Enterprise-21H2.ps1
+++ Win10-Enterprise-21H2.ps1
@@ -12,6 +12,10 @@ Start-Sleep -Seconds 5
 Write-Host -ForegroundColor Green "Start OSDCloud"
 Start-OSDCloud -OSBuild "21H2" -OSLanguage en-us -OSEdition Enterprise -ZTI -OSVersion "Windows 10"
 
+Write-Host "REMOVE USB KEY..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+
 #Restart from WinPE
 #Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
 #Start-Sleep -Seconds 20
